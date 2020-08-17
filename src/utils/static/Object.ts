c18903dcba63b65b04bf0f981b5151f3a26da1e8
Filip Polić <fpolic@profico.hr>
2020-08-17 15:54:19 +0200

    Update .eslintignore, .gitignore, tsconfig.json. Delete static classes with ambiguous names.

--- src/utils/static/Object.ts
+++
@@ -1,6 +0,0 @@
-class Object {
-  static objectHasProperty = (object: Record<string, unknown>, property: string) =>
-    Object.prototype.hasOwnProperty.call(object, property);
-}
-
-export default Object;
