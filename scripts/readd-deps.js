b1a32c53211dab73db9d7bede0bdd0e142a8afae
Filip Polić <fpolic@profico.hr>
2020-11-27 14:47:48 +0100

    Add new script

---
+++ scripts/readd-deps.js
@@ -0,0 +1,24 @@
+/**
+ * This script might be useful if we want to readd all dependencies.
+ * One use-case might be that the version constraints for the packages might be too old and running `yarn upgrade` wouldn't be enough.
+ * Other use-case might be that we explicitly want all the packages to be as newer version as possible. For example, when kicking off a project.
+ */
+
+const path = require('path');
+const childProcess = require('child_process');
+
+const { dependencies, devDependencies } = require(path.resolve(__dirname, '../package.json'));
+
+const listOfDeps = Object.keys(dependencies).join(' ');
+const listOfDevDeps = Object.keys(devDependencies).join(' ');
+
+const removeAllDeps = () => {
+  childProcess.execSync(`yarn remove ${listOfDeps} ${listOfDevDeps}`);
+};
+
+const addAllDeps = () => {
+  childProcess.execSync(`yarn add ${listOfDeps} && yarn add --dev ${listOfDevDeps}`);
+};
+
+removeAllDeps();
+addAllDeps();
